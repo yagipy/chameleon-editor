0d96fdc13bc7f4100c54304f27b6b06d3d993ca5
HiroyukiYagihashi <hey@hiroy.uk>
2021-04-20 00:08:51 +0900

    Modify lint settings

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,7 +9,7 @@ module.exports = {
     sourceType: 'module',
     ecmaVersion: 2020,
     tsconfigRootDir: __dirname,
-    project: ['./tsconfig.json'],
+    project: ['./tsconfig.eslint.json'],
   },
   plugins: ['@typescript-eslint'],
   extends: [
